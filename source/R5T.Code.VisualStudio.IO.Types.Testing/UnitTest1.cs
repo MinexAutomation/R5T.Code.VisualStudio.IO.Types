05159343829311674a726b7eca8d962b084c7f81
MinexAutomation <minex.automation@gmail.com>
2019-06-25 15:32:26 -0400

    Initial commit of source-stub.

---
+++ source/R5T.Code.VisualStudio.IO.Types.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace R5T.Code.VisualStudio.IO.Types.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ source/R5T.Code.VisualStudio.IO.Types/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Code.VisualStudio.IO.Types
+{
+    public class Class1
+    {
+    }
+}
