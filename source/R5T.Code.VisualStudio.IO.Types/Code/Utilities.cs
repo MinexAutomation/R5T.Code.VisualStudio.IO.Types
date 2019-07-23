d055a0abd72abba4edbe5166bf96fca03d5f5f20
MinexAutomation <minex.automation@gmail.com>
2019-07-23 15:56:36 -0400

    Added Visual Studio IO Utilities.

---
+++ source/R5T.Code.VisualStudio.IO.Types/Code/Utilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using PathUtilities = R5T.NetStandard.IO.Paths.Utilities;
+
+
+namespace R5T.Code.VisualStudio.IO
+{
+    public static class Utilities
+    {
+        public static string GetProjectName(string projectFilePath)
+        {
+            var projectName = PathUtilities.GetFileNameWithoutExtension(projectFilePath);
+            return projectName;
+        }
+    }
+}
